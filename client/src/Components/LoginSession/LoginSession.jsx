ac57cbf737359c05b796eb823cc62dbfd15b2eca
Sreerag <sreeragms@cet.ac.in>
2020-09-11 22:42:27 +0530

    login page tweaks

--- client/src/Components/LoginSession/LoginSession.jsx
+++ client/src/Components/LoginSession/LoginSession.jsx
@@ -7,18 +7,18 @@ return (
 <MDBContainer>
   <MDBRow>
     <MDBCol md="9">
-        <div className="cardLogin">
-      <form>
+        <div class="container">
+      <form className="cardLogin">
         <p className="h4 text-center mb-4">Sign in</p>
-        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+        <label htmlFor="emailLogin" className="grey-text">
           Your email
         </label>
-        <input type="email" id="defaultFormLoginEmailEx" className="form-control" />
+        <input type="email" id="emailLogin" className="form-control" />
         <br />
-        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+        <label htmlFor="passwordLogin" className="grey-text">
           Your password
         </label>
-        <input type="password" id="defaultFormLoginPasswordEx" className="form-control" />
+        <input type="password" id="passwordLogin" className="form-control" />
         <div className="text-center mt-4">
           <MDBBtn color="indigo" type="submit">Login</MDBBtn>
         </div>
